87d71bfa44d2413641c51a7e429eec8288a31384
heifade <heifade@126.com>
2017-12-16 20:26:31 +0800

    add test err code

--- test/Update.test.ts
+++ test/Update.test.ts
@@ -184,9 +184,9 @@ describe("Update", function() {
       },
       table: tableName
     })
-      .then(() => {
-        expect(true).to.be.false; // 进到这里就有问题
-      })
+      // .then(() => {
+      //   expect(true).to.be.false; // 进到这里就有问题
+      // })
       .catch(err => {
         let errCode = Reflect.get(err, "code");
         expect(errCode).to.be.equal("ER_TRUNCATED_WRONG_VALUE");
@@ -205,9 +205,9 @@ describe("Update", function() {
       table: tableName,
       where: { id: 2 }
     })
-      .then(() => {
-        expect(true).to.be.false; // 进到这里就有问题
-      })
+      // .then(() => {
+      //   expect(true).to.be.false; // 进到这里就有问题
+      // })
       .catch(err => {
         let errCode = Reflect.get(err, "code");
         expect(errCode).to.be.equal("ER_TRUNCATED_WRONG_VALUE");
