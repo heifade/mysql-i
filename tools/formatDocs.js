053ef264bfb326ca83985f68cdd50808f4fcd89e
heifade <heifade@126.com>
2017-12-22 12:47:03 +0800

    edit docs

--- tools/formatDocs.js
+++ tools/formatDocs.js
@@ -1,61 +1,51 @@
-
 // 替换 doc 目录下面所有"_" 打头的文件
 let fs = require("fs");
+let fsi = require("fs-i");
 
 let renameFileList = [];
-
-// 遍历目录，查找所有文件
-function eachFile(path, callback) {
-  var each = function(path) {
-    if (fs.existsSync(path)) {
-      let files = fs.readdirSync(path);
-      files.forEach((file, index) => {
-        let fileFullName = `${path}/${file}`;
-        if (fs.statSync(fileFullName).isDirectory()) {
-          each(fileFullName);
-        } else {
-          callback({ path, file });
-        }
-      });
-    }
-  };
-
-  each(path);
+function rename(fileName) {
+  return fileName
+    .replace(/\/_/, "/")
+    .replace(/_\//g, "/")
+    .replace(/\/_/g, "/")
+    .replace(/_\./g, ".")
+    .replace(/_/g, '.')
+    // .replace(/_(.)/g, (word, a, b, c) => {
+    //   return a.toUpperCase();
+    // });
 }
 
 // 遍历目录，重命名所有带"_"的文件
-function renameFile(path) {
-  eachFile(path, file => {
-    var fromName = file.file;
-    var toName = file.file.replace(/_/g, "A");
-    fs.renameSync(file.path + "/" + fromName, file.path + "/" + toName);
+async function renameFile(path) {
+  let fileList = await fsi.getAllFiles(path);
+  for (let fileName of fileList) {
+    var toName = rename(fileName);
 
-    if (fromName != toName) {
+    if (fileName != toName) {
+      fs.renameSync(fileName, toName);
       renameFileList.push({
-        fromName,
-        toName
+        fileName: fileName.substr(fileName.lastIndexOf("/") + 1),
+        toName: toName.substr(toName.lastIndexOf("/") + 1)
       });
     }
-  });
+  }
 }
 
 // 在文件中更新文件引用
-function replaceFile(path) {
-  eachFile(path, file => {
-    let fileName = file.path + "/" + file.file;
-    if (
-      fileName.endsWith(".html") ||
-      fileName.endsWith(".js") ||
-      fileName.endsWith(".htm")
-    ) {
+async function replaceFile(path) {
+  let fileList = await fsi.getAllFiles(path);
+  for (let fileName of fileList) {
+    if (fileName.endsWith(".html") || fileName.endsWith(".js") || fileName.endsWith(".htm")) {
       var content = fs.readFileSync(fileName, "utf-8");
       renameFileList.map(h => {
-        content = content.replace(new RegExp(h.fromName, 'g'), h.toName);
+        content = content.replace(new RegExp(h.fileName, "g"), h.toName);
       });
       fs.writeFileSync(fileName, content);
     }
-  });
+  }
 }
 
-renameFile("./docs");
-replaceFile("./docs");
+(async function() {
+  await renameFile("./docs");
+  await replaceFile("./docs");
+})().then();
